56208de155eb280959376ec3863f8c38a209f6f0
uni2008 <93262463+uni2008@users.noreply.github.com>
2022-01-14 14:01:00 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) > -60) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
+radio.setGroup(5)
+radio.setTransmitPower(3)
+basic.forever(function () {
+    radio.sendNumber(0)
+})
